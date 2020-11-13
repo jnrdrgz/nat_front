52e175433f109f586b235e9c6b9ce3f922dd881a
jnrdrgz <jnigrodriguez@gmail.com>
2020-11-13 10:18:29 -0300

    error msg a ciclo y cuota

--- src/components/ABMCuota.js
+++ src/components/ABMCuota.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useLocation, useHistory } from 'react-router-dom';
 import api from "../services/api"
 import "./css/ABMProducto.css"
-
+import ErrorMsg from './ErrorMsg';
 
 const useInput = (defaultValue) => {
     const [value, setValue] = useState(defaultValue);
@@ -16,6 +16,8 @@ const useInput = (defaultValue) => {
 
 const FormComponent = (props) => {
     const monto = useInput(0.0)
+    const [errorMsg, setErrorMsg] = useState("")
+
 
     const addCuota = (e) => {
         e.preventDefault();
@@ -26,15 +28,24 @@ const FormComponent = (props) => {
         }
   
         console.log(payload)
+        
+        if(payload.monto == 0){
+            setErrorMsg("El monto no puede ser 0")
+            return;
+        }
 
         if(props.maximo > monto.value && props.maximo != -1){
             api.put("/pedidos/cliente/pagarCuota", payload).then(r => {
                 console.log(r.data)
                 alert("cuota agregada")
                 //goToConsulta();
-            }).catch(e => console.log(e))
+            }).catch(e => {
+                console.log(e)
+                setErrorMsg("Error en el servidor comuniquese con administrador")
+
+            })
         } else {
-            alert("Error, el monto es mayor al total que falta pagar del pedido")
+            setErrorMsg("Error, el monto es mayor al total que falta pagar del pedido")
         }
   
     } 
@@ -63,6 +74,9 @@ const FormComponent = (props) => {
                 <div className="Titulo">
                     <h3>{props.tipoOperacion}</h3>
                 </div>
+
+                <ErrorMsg errorMsg={errorMsg}/>
+                
                 <form className="form" onSubmit={_onSubmit}>
                     <div className="Inputs">
                         <label>Monto: </label>
